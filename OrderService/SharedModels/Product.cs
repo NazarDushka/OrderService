51b72706e6cebb244b70dcdd8b55bdedcff34454
NazarDushka <dushkanazar@gmail.com>
2024-09-28 12:46:08 +0300

    Created User and Product controllers

--- OrderService/SharedModels/Product.cs
+++ OrderService/SharedModels/Product.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace SharedModels
 {
-    internal class Product
+    public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
     }
 }
